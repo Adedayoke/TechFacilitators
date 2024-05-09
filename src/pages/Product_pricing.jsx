0a1e08a8f7b1f032855888103cb74ec338d1b613
Adedayoke <adedayoke2006@gmail.com>
2024-05-09 10:23:39 +0100

    pricing update

--- src/pages/Product_pricing.jsx
+++ src/pages/Product_pricing.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from "react";
 
 const Product_pricing = () => {
   return (
-    <div>Product_pricing</div>
-  )
-}
+    <div className="product_pricing">
+      <div className="product_pricing--cont">
+        <div className="product_header">
+          <div className="product_intro-desc">
+            <h1>Frontend Web Development</h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque
+              repellendus consectetur incidunt officiis, nemo culpa voluptas
+              inventore natus perspiciatis modi dolorem reprehenderit distinctio
+              labore explicabo suscipit odit, voluptatem quod molestiae.
+            </p>
+            <p>Instructor: Ajayi Victor Damilare</p>
+            <div>
+              <p>English</p>
+            </div>
+          </div>
+        </div>
+        <div className="product_overview">
+          <img src="" alt="" />
+          <p>$100</p>
+          <button>Enroll</button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Product_pricing
+export default Product_pricing;
