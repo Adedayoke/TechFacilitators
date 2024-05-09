25e8d1db9c4821e181666599606dcec62f772550
Adedayoke <adedayoke2006@gmail.com>
2024-05-09 13:15:34 +0100

    pricing update

--- src/pages/Product_pricing.jsx
+++ src/pages/Product_pricing.jsx
@@ -1,6 +1,17 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import image2 from "../images/Backend.svg";
 
 const Product_pricing = () => {
+  const [fixed_state, setfixedState] = useState(false);
+  window.onscroll = function () {
+    const presentView = document.documentElement.scrollTop;
+    console.log(presentView);
+    if (presentView > 390) {
+      setfixedState(true);
+    } else if (presentView < 390) {
+      setfixedState(false);
+    }
+  };
   return (
     <div className="product_pricing">
       <div className="product_pricing--cont">
@@ -19,10 +30,88 @@ const Product_pricing = () => {
             </div>
           </div>
         </div>
-        <div className="product_overview">
-          <img src="" alt="" />
-          <p>$100</p>
-          <button>Enroll</button>
+        <div className="product_body">
+          <div className="product_body-section1 section">
+            <h2>What you will learn.</h2>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure
+              vero necessitatibus, esse excepturi voluptatibus exercitationem
+              error recusandae sequi autem dolore accusantium fugiat ea.
+              Voluptates vero corrupti dolor necessitatibus corporis numquam?
+            </p>
+          </div>
+          <div className="product_body-section2 section">
+            <h2>Content</h2>
+            <div className="content_container">
+                <div>
+                  <h3>
+                    <button>
+                      <span>
+                        <span>
+                          Day 1 - Beginner - Working with Variables in Python to
+                          Manage Data
+                        </span>
+                        <span>
+                          17 lectures • <span>1hr 35min</span>
+                        </span>
+                      </span>
+                    </button>
+                  </h3>
+                  <svg>
+                  </svg>
+                </div>
+                <div aria-hidden="true">
+                    <ul>
+                      <li>
+                        <div>
+                          <svg>
+                          </svg>
+                          <div>
+                            <div>
+                              <div>
+                                <button>
+                                  <span>
+                                    What you're going to get from this course
+                                  </span>
+                                </button>
+                              </div>
+                            </div>
+                            <button>
+                              <span>
+                                Preview
+                              </span>
+                            </button>
+                            <span>
+                              03:27
+                            </span>
+                          </div>
+                        </div>
+                      </li>
+                    </ul>
+                </div>
+            </div>
+          </div>
+        </div>
+        <div
+          className={
+            fixed_state
+              ? "product_overview overviewAnimationContf"
+              : "product_overview"
+          }
+        >
+          <img src={image2} alt="" />
+          <h1>$100</h1>
+          <button className="applyBtn">
+            <h3>Apply</h3>
+          </button>
+          <h3>This course includes:</h3>
+          <ul>
+            <li>Lorem, ipsum.</li>
+            <li>Lorem, ipsum.</li>
+            <li>Lorem, ipsum.</li>
+            <li>Lorem, ipsum.</li>
+            <li>Lorem, ipsum.</li>
+          </ul>
         </div>
       </div>
     </div>
